712f7ab16fe90ef7be4f12ca0f2cb7988ba7503b
arteeh <arteehlive@gmail.com>
2019-03-26 23:04:44 +0100

    brr

--- about.php
+++ about.php
@@ -5,6 +5,7 @@
 	<p>
 		I'm Maarten de Jong (but online I'm arteeh), and you are looking at my website. Currently I'm a computer science student at Hogeschool Windesheim in Zwolle and i spend most of my time on that. Besides school, i like to play video games like Skyrim, Mass Effect and Destiny (1, but that doesn't need to be said). I also like to program and read books. I am mostly still figuring out what I'm gonna do with my life, but i think i'm getting close. I hope to document this journey on my blog on the homepage, which i'm pretty excited for. I've always wanted to write.
 	</p>
+	<br>
 	<p>
 		If you wanna get in touch, you DM me on my twitter which you can find on the bottom left of this page. If you want, you can also add me on discord: arteeh#3348. See you around.
 </div>

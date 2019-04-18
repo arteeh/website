3353a2dc1c781c621593b11df9b2dd9e954a5008
arteeh <arteehlive@gmail.com>
2019-04-18 18:52:03 +0200

    restore old stuff

--- about.php
+++ about.php
@@ -2,51 +2,39 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>arteeh's website</title>
+	<title>arteeh's online property</title>
 	<meta name="description" content="arteeh's personal website">
-	<meta name="theme-color" content="#F7F8F9">
+	<meta name="theme-color" content="#1a1a1a">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="icon" href="src/favicon.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="favicon.png">
 </head>
 <body>
-	<div id="container">
-		<div id="header">
-			<div>
-				<span class="nav">
-					&nbsp;
-					[<a href="index.php">blog</a>]
-					&nbsp;
-					[<a href="downloads.php">downloads</a>]
-					&nbsp;
-					[<a href="about.php">about me</a>]
-				</span>
-			</div>
-		</div>
-		
-		<hr>
-		
-		<div id="body">
-			<div>
-				<p>I'm Maarten de Jong (but online I'm arteeh), and you are looking at my website. Currently I'm a computer science student at Hogeschool Windesheim in Zwolle and i spend most of my time on that. Besides school, i like to play video games like Skyrim, Mass Effect and Destiny (1, but that doesn't need to be said). I also like to program and read books. I am mostly still figuring out what I'm gonna do with my life, but i think i'm getting close. I hope to document this journey on my blog on the homepage, which i'm pretty excited for. I've always wanted to write.</p>
-				<br>
-				<p>If you wanna get in touch, you DM me on my twitter which you can find on the bottom left of this page. If you want, you can also add me on discord: arteeh#3348. See you around.</p>
-			</div>
+	<div id="body">
+	
+		<div>
+			<p>I'm Maarten de Jong (but online I'm arteeh), and you are looking at my website. Currently I'm a computer science student at Hogeschool Windesheim in Zwolle and i spend most of my time on that. Besides school, i like to play video games like Skyrim, Mass Effect and Destiny (1, but that doesn't need to be said). I also like to program and read books. I am mostly still figuring out what I'm gonna do with my life, but i think i'm getting close. I hope to document this journey on my blog on the homepage, which i'm pretty excited for. I've always wanted to write.</p>
+			<br>
+			<p>If you wanna get in touch, you DM me on my twitter which you can find on the bottom left of this page. If you want, you can also add me on discord: arteeh#3348. See you around.</p>
 		</div>
 
-		<div id="footer">
-			
-			<hr>
-			
-			<div class='footer'>
-				<div class='footerContent'>
-					&nbsp;
-					[<a href="https://www.twitter.com/arteehlive">twitter</a>]
-					&nbsp;
-					[<a href="https://www.instagram.com/arteehlive">instagram</a>]
-					&nbsp;
-					[<a href="https://gitlab.com/arteeh/website">source</a>]
-				</div>
+	</div>
+	
+	<div id="footer">
+	
+		<hr>
+
+		<div class='footer'>
+			<div class='footerContent'>
+				[<a href="index.php">blog</a>]
+				&nbsp;
+				[<a href="about.php">about me</a>]
+				&nbsp;
+				[<a href="https://www.twitter.com/arteehlive">twitter</a>]
+				&nbsp;
+				[<a href="https://www.instagram.com/arteehlive">instagram</a>]
+				&nbsp;
+				[<a href="https://gitlab.com/arteeh/website">sauce</a>]
 			</div>
 		</div>
 	</div>
--- index.php
+++ index.php
@@ -2,49 +2,35 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>arteeh's website</title>
+	<title>arteeh's online property</title>
 	<meta name="description" content="arteeh's personal website">
-	<meta name="theme-color" content="#F7F8F9">
+	<meta name="theme-color" content="#1a1a1a">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="icon" href="src/favicon.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="icon" href="favicon.png">
 </head>
 <body>
-	<div id="container">
-		<div id="header">
-			<div>
-				<span class="nav">
-					&nbsp;
-					[<a href="index.php">blog</a>]
-					&nbsp;
-					[<a href="downloads.php">downloads</a>]
-					&nbsp;
-					[<a href="about.php">about me</a>]
-				</span>
-			</div>
-		</div>
-		
+	<div id="body">
+	
+	welcome.
+	
+	</div>
+	
+	<div id="footer">
+	
 		<hr>
-		
-		<div id="body">
-		
-		welcome.
-		
-		</div>
 
-		<div id="footer">
-			
-			<hr>
-			
-			<div class='footer'>
-				<div class='footerContent'>
-					&nbsp;
-					[<a href="https://www.twitter.com/arteehlive">twitter</a>]
-					&nbsp;
-					[<a href="https://www.instagram.com/arteehlive">instagram</a>]
-					&nbsp;
-					[<a href="https://gitlab.com/arteeh/website">source</a>]
-				</div>
+		<div class='footer'>
+			<div class='footerContent'>
+				[<a href="index.php">blog</a>]
+				&nbsp;
+				[<a href="about.php">about me</a>]
+				&nbsp;
+				[<a href="https://www.twitter.com/arteehlive">twitter</a>]
+				&nbsp;
+				[<a href="https://www.instagram.com/arteehlive">instagram</a>]
+				&nbsp;
+				[<a href="https://gitlab.com/arteeh/website">sauce</a>]
 			</div>
 		</div>
 	</div>
